ca611aa007d6f66f00006fc1ea8cf2c6a1c65d3d
orangefibernet <orangeultraband@gmail.com>
2026-07-13 04:05:14 +0000

    feat(frontend): initialize OrangeERP frontend foundation

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,22 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-import {
-  CssBaseline,
-  ThemeProvider,
-} from '@mui/material';
+import App from "./App";
 
-import App from './App';
-import theme from './theme/theme';
-
-ReactDOM.createRoot(
-  document.getElementById('root')!,
-).render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>,
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
 );
